039e4ece82e2b5d9aac8f7f4973dfe67541ae33d
M <mrgmckenzie@gmail.com>
2024-02-01 11:14:07 +0000

    finished home page exercise

--- Home.js
+++ Home.js
@@ -4,18 +4,12 @@ import {
   StyleSheet,
   Text,
   View,
-  SafeAreaView,
   Image,
-  TouchableWithoutFeedback,
-  TouchableOpacity,
-  Button,
-  Alert,
   Platform,
   Dimensions,
 } from "react-native";
-import Home from "./Home";
 
-export default function App() {
+export default function Home() {
   const handlePress = () => console.log("Text clicked!");
 
   dimensions = Dimensions.get("screen");
@@ -47,53 +41,8 @@ export default function App() {
         />
         <Text>Sell What You Don't Need</Text>
       </View>
-
       <View style={[styles.button, styles.coral]}></View>
       <View style={[styles.button, styles.aqua]}></View>
-
-      {/* <View
-        style={{
-          width: orientation === "landscape" ? "100%" : "90%",
-          height: orientation === "landscape" ? "100%" : "30%",
-          backgroundColor: "darkorange",
-        }}
-      >
-        <Text style={styles.text}>ORANGE</Text>
-      </View> */}
-      {/* <TouchableOpacity onPress={() => console.log("Image tap!")}>
-        <Image
-          source={{
-            width: 400,
-            height: 400,
-            uri: "https://picsum.photos/401",
-          }}
-        />
-      </TouchableOpacity> */}
-      {/* <Image source={require('./assets/splash.png')}></Image> */}
-      {/* <Text numberOfLines={5} onPress={handlePress}>
-        Hello World!
-      </Text>
-      <Button
-        title="Do not press"
-        color={"darkorange"}
-        onPress={() =>
-          Alert.alert("WARNING!", "I told you not to press that...", [
-            { text: "Run away", onPress: () => console.log("You ran away.") },
-            {
-              text: "Do nothing",
-              onPress: () => console.log("You did nothing."),
-            },
-          ])
-        }
-      ></Button> */}
-      {/* <StatusBar style="auto" /> */}
-      {/* <View style={{ width: 100, height: 100, backgroundColor: "gold" }} />
-      <View style={{ width: 100, height: 130, backgroundColor: "crimson" }} />
-      <View style={{ width: 100, height: 160, backgroundColor: "deeppink" }} />
-      <View style={{ width: 100, height: 190, backgroundColor: "turquoise" }} />
-      <View
-        style={{ width: 100, height: 220, backgroundColor: "greenyellow" }}
-      /> */}
     </View>
   );
 }
@@ -104,9 +53,7 @@ const styles = StyleSheet.create({
     flexDirection: "column", // the primary axis
     justifyContent: "flex-start", //applies to the primary axis
     // alignItems: "baseline", //applies to the secondary axis - each single line is aligned this way (in this case baseline aligned)
-    // flexWrap: "wrap",
     alignContent: "center", //applies to the content as a whole (all the content is centered)
-    backgroundColor: "grey",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
   logo: {
